b07b83c2b37680ab23c7cae18eec468d55d6a662
Omar Einea <eineao@gmail.com>
2019-02-12 19:51:32 +0400

    Step 3: add a Card in a Column

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,23 @@ import 'package:flutter/material.dart';
 main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: Text('Flutter Workshop')
         ),
-        body: Center(
-          child: Text('Welcome'),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Card(
+              margin: EdgeInsets.all(8),
+              elevation: 2,
+              child: Padding(
+                padding: EdgeInsets.all(8),
+                child: Text('welcome'),
+              )
+            ),
+          ]
         ),
       )
     )
