1c0a1f6e01f63f0cd76012523dca3227c5232c3e
Omar Einea <eineao@gmail.com>
2019-02-13 11:32:16 +0400

    Step 6: infinite list of numbers with SnackBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
 
 main() {
-  var card = (text) => Card(
+  var card = (context, text) => Card(
     margin: EdgeInsets.fromLTRB(8, 4, 8, 4),
     elevation: 2,
     child: InkWell(
-      onTap: () { print(text.toString()); },
+      onTap: () {
+        Scaffold.of(context).showSnackBar(
+          SnackBar(
+            content: Text('You tapped card number ' + text.toString())
+          )
+        );
+      },
       child: Padding(
         padding: EdgeInsets.all(16),
         child: Container(
           width: double.infinity,
-          child: Text(text.toString(), textScaleFactor: 1.2)
+          child: Text('Card number ' + text.toString(), textScaleFactor: 1.2)
         ),
       )
     )
@@ -23,15 +29,7 @@ main() {
         appBar: AppBar(
           title: Text('Flutter Workshop')
         ),
-        body: ListView(
-          // crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            card('hello'),
-            card('hi'),
-            card('hoi'),
-            card('hey')
-          ]
-        ),
+        body: ListView.builder(itemBuilder: (context, index) => card(context, index))
       )
     )
   );
