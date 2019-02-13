a29cc90fbadbdb3bcaa80e307a03dac21cea3155
Omar Einea <eineao@gmail.com>
2019-02-13 11:25:50 +0400

    Step 5: print text when Card is tapped

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,18 @@ import 'package:flutter/material.dart';
 
 main() {
   var card = (text) => Card(
-      margin: EdgeInsets.fromLTRB(8, 4, 8, 4),
-      elevation: 2,
+    margin: EdgeInsets.fromLTRB(8, 4, 8, 4),
+    elevation: 2,
+    child: InkWell(
+      onTap: () { print(text.toString()); },
       child: Padding(
         padding: EdgeInsets.all(16),
-        child: Text(text, textScaleFactor: 1.2,),
+        child: Container(
+          width: double.infinity,
+          child: Text(text.toString(), textScaleFactor: 1.2)
+        ),
       )
+    )
   );
 
   runApp(
